6f4b5ee4bc2b4593255d2d65d098dbfdef319ec5
HoonWi <wee0702@gmail.com>
2024-01-10 18:46:24 +0900

    Feat: openweatherapi API 연동

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,15 @@
+const API_KEY = "75f0b43f95bbf3ff2636621ef8a4e145"
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log(lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  fetch(url).then(response => response.json()).then(data => {
+    const weather = document.querySelector("#weather span:first-child")
+    const city = document.querySelector("#weather span:last-child")
+    weather.innerText = `${data.weather[0].mai} / ${Math.floor(data.main.temp)}`;
+    city.innerText = data.name;
+  })
 }
 function onGeoError() {
   console.log("Can't find you. No weather for you.")
